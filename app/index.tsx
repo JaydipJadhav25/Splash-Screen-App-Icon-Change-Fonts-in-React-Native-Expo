addfef670e8fe8c9c6a3c46c645e7e02aeef3307
jaydipjadhav <jaydipdjadhav>
2024-10-02 12:29:31 +0530

    add expo google font and useFont hook checking loading state

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,27 @@
 import { Text, View } from "react-native";
+import { Inter_900Black, useFonts } from '@expo-google-fonts/inter';
+import * as SplashScreen from 'expo-splash-screen';
+import {useEffect} from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Index() {
+
+  const [ loadedFount] = useFonts({
+    Inter_900Black
+  });
+
+  console.log("loaded fount : " , loadedFount);
+
+//check fount if page in show / mounted
+  useEffect(() =>{
+    if(loadedFount){
+      //fount asel tr not show page 
+      SplashScreen.hideAsync();
+    }
+
+  } ,[loadedFount])
+
   return (
     <View
       style={{
@@ -9,7 +30,11 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Jaydip Dhananajay Jadhav</Text>
+      <Text
+      style={{
+        fontFamily: 'Inter_900Black'
+      }}
+      >Jaydip Dhananajay Jadhav</Text>
     </View>
   );
 }
